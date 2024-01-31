50f2ef0c7e8b0367310eb583ba527cf2c318fff3
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-01-31 08:34:52 -0300

    feat: adds marvelFont

---
+++ src/components/Fonts/Marvel/marvel.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local"
+
+export const marvelFont = localFont({
+  src: [
+    {
+      path: "./MarvelRegular-Dj83.ttf",
+      weight: "400",
+      style: "normal",
+    }
+  ],
+})
