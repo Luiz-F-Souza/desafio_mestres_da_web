efd6b33f628b54a377c67bcfa023eee638c4bc14
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-01 08:58:27 -0300

    feat: creates movies page

--- src/app/filmes/page.tsx
+++ src/app/filmes/page.tsx
@@ -1,6 +1,50 @@
-export default function MoviesPage() {
-    return (
-      <main></main>
-    );
-  }
-  
+import { getHerosDetails } from "@/api-functions/heros/getDetails"
+import { getAllMovies } from "@/api-functions/movies/getAll"
+import { InformationCard } from "@/components/Cards/InformationCard"
+import { HeroMoreDetails } from "@/components/Cards/components/HeroMoreDetails"
+import { ScrollableContainer } from "@/components/ScrollableContainer"
+
+type Props = {
+  searchParams: { slug: string } | undefined
+}
+
+export default async function MoviesPage({ searchParams }: Props) {
+  const response = await getAllMovies()
+  const allMovies = response.data
+
+  const slug = searchParams?.slug
+  const isSomeCardOpen = !!slug
+
+  // const movieMoreDetailsRequest = isSomeCardOpen
+  //   ? await getHerosDetails({ slug: slug })
+  //   : undefined
+
+  // const movieDetails = movieMoreDetailsRequest?.data.detail
+
+  return (
+    <main className="flex flex-col flex-1">
+      <ScrollableContainer>
+        {allMovies?.map((movie) => {
+          const isCurrentCardOpen = slug === movie.nameSlug
+          return (
+            <InformationCard
+              key={movie.id}
+              description={movie.description}
+              cardSlug={movie.nameSlug}
+              title={movie.name}
+              imageURL={movie.imageUrl}
+              pageSlug={searchParams?.slug}
+            >
+              TESTEE
+              {/* <HeroMoreDetails
+                isOpen={isCurrentCardOpen}
+                title={movie.name}
+                heroDetails={isCurrentCardOpen ? heroDetails : undefined}
+              /> */}
+            </InformationCard>
+          )
+        })}
+      </ScrollableContainer>
+    </main>
+  )
+}
