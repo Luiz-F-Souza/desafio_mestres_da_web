ffce25a442f00fd89c95b47e4e344170973ba871
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-01 01:54:00 -0300

    fix: makes logo more fit to phones

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,9 +2,29 @@ import { marvelFont } from "./Fonts/Marvel/marvel"
 
 export const Logo = () => {
   return (
-    <div className="relative hover:animate-pulse hover:duration-500">
-      <div className="bg-red-500 w-40 h-16 absolute -z-10 hover:animate-pulse hover:duration-500" />
-      <h2 className={`text-8xl ml-7 tracking-widest  ${marvelFont.className}`}>MARVEL</h2>
+    <div
+      className="
+      relative hover:animate-pulse hover:duration-500
+      "
+    >
+      <div
+        className="
+          bg-red-500 
+          w-32 lg:w-40 
+          h-12 lg:h-16 
+          absolute -z-10 
+          "
+      />
+      <h2
+        className={`
+          text-6xl lg:text-8xl
+          ml-7 
+          tracking-widest  
+          ${marvelFont.className}
+        `}
+      >
+        MARVEL
+      </h2>
     </div>
   )
 }
