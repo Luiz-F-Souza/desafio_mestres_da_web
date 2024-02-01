106de1610d082bb1ad709aed76eb37144e9602fe
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-01 08:56:33 -0300

    feat: create type for Movie

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -12,3 +12,13 @@ export type HeroDetails = {
   movies: string[]
   rate: 0 | 1 | 2 | 3 | 4 | 5
 }
+
+export type Movie = {
+  id: number
+  name: string
+  nameSlug: string
+  description: string
+  launchedIn: string
+  chronologyOrder: number
+  imageUrl?: string
+}
