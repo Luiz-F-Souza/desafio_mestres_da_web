9e070e0f8ac3df1349909c672d431875f0e305e1
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-01 01:48:40 -0300

    feat: adds axiforma fonts

---
+++ src/components/Fonts/Axiforma/axiforma.ts
@@ -0,0 +1,21 @@
+import localFont from "next/font/local"
+
+export const axiformaBold = localFont({
+  src: [
+    {
+      path: "./fonts/Kastelov - Axiforma Bold.otf",
+      weight: "700",
+      style: "bold",
+    }
+  ],
+})
+
+export const axiformaLight = localFont({
+  src: [
+    {
+      path: "./fonts/Kastelov - Axiforma Bold.otf",
+      weight: "300",
+      style: "light",
+    }
+  ],
+})
