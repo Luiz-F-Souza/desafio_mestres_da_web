acc03c9226705e85aa8231ea5380cdcf89462006
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-01 08:58:57 -0300

    feat: uses the new version of InformationCard

--- src/app/personagens/page.tsx
+++ src/app/personagens/page.tsx
@@ -1,5 +1,7 @@
 import { getAllHeros } from "@/api-functions/heros/getAllHeros"
-import { IntroCard } from "@/components/Cards/IntroCard"
+import { getHerosDetails } from "@/api-functions/heros/getDetails"
+import { InformationCard } from "@/components/Cards/InformationCard"
+import { HeroMoreDetails } from "@/components/Cards/components/HeroMoreDetails"
 import { ScrollableContainer } from "@/components/ScrollableContainer"
 
 type Props = {
@@ -10,12 +12,35 @@ export default async function HerosPage({ searchParams }: Props) {
   const response = await getAllHeros()
   const allHeros = response.data
 
+  const slug = searchParams?.slug
+  const isSomeCardOpen = !!slug
+
+  const heroMoreDetailsRequest = isSomeCardOpen
+    ? await getHerosDetails({ slug: slug })
+    : undefined
+
+  const heroDetails = heroMoreDetailsRequest?.data.detail
+
   return (
     <main className="flex flex-col flex-1">
       <ScrollableContainer>
         {allHeros?.map((hero) => {
+          const isCurrentCardOpen = slug === hero.nameSlug
           return (
-            <IntroCard key={hero.id} hero={hero} slug={searchParams?.slug} />
+            <InformationCard
+              key={hero.id}
+              description={hero.description}
+              cardSlug={hero.nameSlug}
+              title={hero.name}
+              imageURL={hero.imageUrl}
+              pageSlug={searchParams?.slug}
+            >
+              <HeroMoreDetails
+                isOpen={isCurrentCardOpen}
+                title={hero.name}
+                heroDetails={isCurrentCardOpen ? heroDetails : undefined}
+              />
+            </InformationCard>
           )
         })}
       </ScrollableContainer>
