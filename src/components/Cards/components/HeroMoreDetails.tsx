2c132a7ea99050dd366cbd1d60e5226a99409c4d
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-02 00:37:27 -0300

    fix: makes responsiveness

--- src/components/Cards/components/HeroMoreDetails.tsx
+++ src/components/Cards/components/HeroMoreDetails.tsx
@@ -11,39 +11,41 @@ type Props = {
 export const HeroMoreDetails = ({ isOpen, title, heroDetails }: Props) => {
   const rate = heroDetails?.rate ?? 0
   return (
-    <section
-      className={`
-          bg-gradient-to-b from-red-400 to-red-700
-          absolute top-0 bottom-0 left-48 right-0
-          z-40
-          pr-8 pl-36 py-14
-          ${isOpen ? "visible " : "invisible"} 
-        `}
-    >
-      <h3
+    <>
+      <section
         className={`
-            text-left mb-4 text-3xl line-clamp-1 text-ellipsis
+          z-10
+          rounded-3xl rounded-tl-none rounded-bl-none
+          px-5 py-14
+          flex flex-col
+          transition-[opacity] duration-1000
+          ${isOpen ? "opacity-1 " : "opacity-0"} 
+        `}
+      >
+        <h3
+          className={`
+            text-left text-xl md:text-3xl md:line-clamp-1 text-ellipsis
             ${axiformaBold.className}
           `}
-      >
-        {title}
-      </h3>
-      <div>
-        <h4>Aparições:</h4>
-        <ul className="text-left">
-          {heroDetails?.movies.slice(0, 6).map((movie) => {
-            return (
-              <li key={movie} className="line-clamp-1 text-ellipsis">
-                {movie}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+        >
+          {title}
+        </h3>
+        <div className="mb-auto">
+          <h4>Aparições:</h4>
+          <ul className="text-left text-sm md:text-base list-disc list-inside md:list-none">
+            {heroDetails?.movies.slice(0, 6).map((movie) => {
+              return (
+                <li key={movie} className="md:line-clamp-1 text-ellipsis">
+                  {movie}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
 
-      <RateSection title="Avaliação dos Fãs" rate={rate} />
-
-      <CloseCardButton className="flex w-full justify-end mt-2" />
-    </section>
+        <RateSection title="Avaliação dos Fãs" rate={rate} />
+      </section>
+      {isOpen && <CloseCardButton />}
+    </>
   )
 }
