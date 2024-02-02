8e5077b1e7707ef6b5fe7adf4d8006b5b05c20b2
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-02-02 09:02:23 -0300

    feat: creates filter

--- src/api-functions/movies/getAll.ts
+++ src/api-functions/movies/getAll.ts
@@ -2,21 +2,47 @@ import { Movie } from "@/@types/types"
 import { BASE_API_URL } from "@/env/CONSTANTS"
 // import { env } from "@/env"
 
-export async function getAllMovies(): Promise<{ data: Movie[] } | null> {
+type Props = {
+  orderBy: string
+}
+
+type ResponseType = { data: Movie[] } | null
+export async function getAllMovies({ orderBy }: Props): Promise<ResponseType> {
   if (!BASE_API_URL) return null
-  const response = await fetch(
-    `${BASE_API_URL}/api/movie/get-all`,
-    {
-      headers: {
-        Accept: "application/json",
-        method: "GET",
-      },
-    }
-  )
+  const response = await fetch(`${BASE_API_URL}/api/movie/get-all`, {
+    headers: {
+      Accept: "application/json",
+      method: "GET",
+    },
+  })
 
   if (!response.ok) {
     throw new Error("Erro ao consultar os dados dos filmes")
   }
 
-  return response.json()
+  const data: ResponseType = await response.json()
+
+  const movies = data?.data
+
+  if (!movies) return null
+
+  const doesOrderByHasValidValue =
+    orderBy === "chronologyOrder" || orderBy === "launchedIn"
+  if (!doesOrderByHasValidValue) orderBy = "launchedIn"
+
+  const filteredMovies = movies?.sort((movieA, movieB) => {
+    if (orderBy === "chronologyOrder")
+      return movieA.chronologyOrder - movieB.chronologyOrder
+
+    if (orderBy === "launchedIn") {
+      const dateMovieA = new Date(movieA.launchedIn).getMilliseconds()
+      const dateMovieB = new Date(movieB.launchedIn).getMilliseconds()
+
+      return dateMovieA - dateMovieB
+    }
+
+    return 0
+  })
+
+  return { data: filteredMovies }
 }
